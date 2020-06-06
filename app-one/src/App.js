3870bf5e87603a319c881ee1aa112c600baf92a7
Nabeel-Chhipa <58781270+Nabeel-Chhipa@users.noreply.github.com>
2020-06-07 02:05:17 +0500

    update react component
    update react function component

---
+++ app-one/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import FunComp from './Components/FunCom';
+
+function App() {
+  return (
+    <div>
+      <FunComp />
+    </div>
+  );
+}
+
+export default App;
